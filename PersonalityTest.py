e06b0f23524408ad965de304721ce3998a3cd528
Josh <josh.benson.805@gmail.com>
2019-09-18 10:22:42 -0700

    Addded More Questions

--- PersonalityTest.py
+++ PersonalityTest.py
@@ -1,13 +1,19 @@
 from tkinter import *
 num = 0
-agreeability = 0
-extraversion = 0
-judgability = 0
 
 QA1 = ["You hate cats. Like hate hate them. Someone is telling you about how much you love cats and ask you what you think of cats. How do you respond?", "I hate cats.", "I'm more of a dog person but I still like cats.", "I love cats!"]
-QA2 = ["You and your'e friend are in a debate about politics. You tell him a fact is wrong and ou decide to look it up as proof. You end up being right. You say:", "Ha I told you so.", "Yeah I read about this last week.", "I wasn't sure either."]
+QA2 = ["You and your'e friend are in a debate about politics. You tell him a fact is wrong and you decide to look it up as proof. You end up being right. You say:", "Ha I told you so.", "Yeah I read about this last week.", "I wasn't sure either."]
 QA3 = ["You LOVE Juicy Fruit Gum. You take out your gum in class and an classmate next to you asks for a peice.", "Don't give him one.", "Give him one as long as it's not the last peice.", "Give him one."]
-
+QA4 = ["If you had the choice between these super powers you would choose:", "invisibility", "mind-reading", "telekenis"]
+QA5 = ["It's saturday night. Choose between the following:", "Hang out with your best friend", "Watch a movie with 4 friends", "Go to a giant house party"]
+QA6 = ["What is most important in high school", "Makin Paper", "Good grades", "Fun"]
+QA7 = ["What is the best university", "Harvard", "Stanford", "MIT"]
+QA8 = ["What is the best girl to guy ratio at a party", "30%", "50%", "70%"]
+QA9 = ["What is the best color", "Blue", "Red", "Thats a lame question"]
+QA10 = ["Out of the following you would rather do:", "Surfing", "Minecraft Bed Wars", "Gardening"]
+Q11 = ["You are talking with your friend about which of the following:", "Girls/boys", "Politics/Philosophy", "School"]
+QA12 = ["You listen to:", "Rap", "Country", "Classic Rock"]
+QA13 = ["This quiz is", "Awesome", "Okay", "Dumb"]
 
 qList = [QA1, QA2, QA3]
 currentSet = qList[num]
